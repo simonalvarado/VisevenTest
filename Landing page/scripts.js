3ea4f0c3d722a808be68e6b0a4a2337d6dbfd21e
Simon Alvarado <simonjesusalvarado@gmail.com>
2022-06-13 02:20:46 -0300

    Update of the landing page

--- Landing page/scripts.js
+++ Landing page/scripts.js
@@ -1,34 +1,34 @@
 //hamburguer menu function
-const open_hamburguer = document.getElementById('open_hamburguer');
-const close_hamburguer = document.getElementById('close_hamburguer');
-const my_links = document.getElementById("my_links");
-open_hamburguer.addEventListener('click', () => {
-  my_links.style.display = "block";
-  open_hamburguer.style.display = "none";
-  close_hamburguer.style.display = "block";
+const openHamburguer = document.getElementById('openHamburguer');
+const closeHamburguer = document.getElementById('closeHamburguer');
+const myLinks = document.getElementById("myLinks");
+openHamburguer.addEventListener('click', () => {
+  myLinks.style.display = "block";
+  openHamburguer.style.display = "none";
+  closeHamburguer.style.display = "block";
   body.classList.add('fixed')
 })
-close_hamburguer.addEventListener('click', () => {
-  my_links.style.display = "none";
-  open_hamburguer.style.display = "block";
-  close_hamburguer.style.display = "none";
+closeHamburguer.addEventListener('click', () => {
+  myLinks.style.display = "none";
+  openHamburguer.style.display = "block";
+  closeHamburguer.style.display = "none";
   body.classList.remove('fixed')
 })
 
 // script for the pop-up
 const body = document.body;
-const popup_container = document.getElementById('popup_container');
+const popupContainer = document.getElementById('popupContainer');
 const close = document.getElementById('close');
 
 close.addEventListener('click', () => {
-  popup_container.classList.remove('show')
+  popupContainer.classList.remove('show')
   body.classList.remove('fixed')
 })
 
 // script for the validation
-const form = document.getElementById('form');
-const phone_number = document.getElementById('phone_number');
-const day_rate = document.getElementById('day_rate');
+const form = document.getElementById('form1');
+const phoneNumber = document.getElementById('phoneNumber');
+const dayRate = document.getElementById('dayRate');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -38,58 +38,61 @@ form.addEventListener('submit', (e) => {
 
 // checking the inputs
 function checkInputs() {
-  const phone_number_value = phone_number.value.trim();
-  const day_rate_value = day_rate.value.trim();
+  const phoneNumberValue = phoneNumber.value.trim();
+  const dateRateValue = dayRate.value.trim();
+  let phoneNumberSuccess;
+  let dateRateSuccess;
 
-  if(!phone_number_value.length) {
-    setErrorFor(phone_number);
-    var phone_number_success = false;
+  if(!phoneNumberValue.length) {
+    setErrorFor(phoneNumber);
+    phoneNumberSuccess = false;
   } else {
-    setSuccessFor(phone_number);
-    var phone_number_success = true;
+    setSuccessFor(phoneNumber);
+    phoneNumberSuccess = true;
   }
 
-  if(day_rate_value.length === 0) {
-    setErrorFor(day_rate);
-    var date_rate_success = false;
+  if(dateRateValue.length === 0) {
+    setErrorFor(dayRate);
+    dateRateSuccess = false;
   } else {
-    setSuccessFor(day_rate);
-    var date_rate_success = true;
+    setSuccessFor(dayRate);
+    dateRateSuccess = true;
   }
 
-  if(phone_number_success && date_rate_success) {
-    document.getElementById('popup_paragraph').innerHTML = `Your phone number is: ${phone_number_value} <br> Your day rate is: ${day_rate_value}`;
-    popup_container.classList.add('show');
+  if(phoneNumberSuccess && dateRateSuccess) {
+    document.getElementById('popupParagraph').innerHTML = `Your phone number is: ${phoneNumberValue} <br> Your day rate is: ${dateRateValue}`;
+    popupContainer.classList.add('show');
     body.classList.add('fixed')
   }
 }
 
 //functions for style of the inputs
+let formControl;
 function setErrorFor(input) {
-  const formControl = input.parentElement;
+  formControl = input.parentElement;
 
   formControl.className = 'form-control error';
 }
 function setSuccessFor(input) {
-  const formControl = input.parentElement;
+  formControl = input.parentElement;
 
   formControl.className = 'form-control success';
 }
 
 // Genereting options
 const form2 = document.querySelector('#form2');
-document.querySelector('#number-employees').oninput = () => {
+document.querySelector('#numberEmployees').oninput = () => {
 
-  let number_employees = document.getElementById('number-employees');
-  const number_employees_value = number_employees.value.trim();
+  let numberEmployees = document.getElementById('numberEmployees');
+  const numberEmployeesValue = numberEmployees.value.trim();
 
-  form2.payroll_select.innerHTML = "";
+  form2.payrollSelect.innerHTML = "";
 
-  for(let i = 0; i <= number_employees_value; i++){
-    let opcion = document.createElement('option');
-    opcion.value = i;
-    opcion.innerText = i;
-    form2.payroll_select.appendChild(opcion);
+  for(let i = 0; i <= numberEmployeesValue; i++){
+    let option = document.createElement('option');
+    option.value = i;
+    option.innerText = i;
+    form2.payrollSelect.appendChild(option);
   }
 }
 
